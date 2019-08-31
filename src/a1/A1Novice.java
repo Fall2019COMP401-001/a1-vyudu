cb700e886a474df94d5954aceab2bad556a84d00
vydu <vydu@yingliangs-air.wireless-1x.unc.edu>
2019-08-31 12:53:35 -0400

    finish

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,30 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		int a = 0; 
+		int customers = scan.nextInt();
+		
+		while (a < customers) {
+			
+			double price = 0.0; 
+			char initial1 = scan.next().charAt(0);
+			String surname1 = scan.next();
+			
+			int totItem = scan.nextInt();
+			int i = 0;
+			
+			while (i < totItem) {
+				price = price + scan.nextInt() * scan.nextDouble();
+				i++;
+			}
+			
+			Double.toString(price);
+			
+			System.out.println(initial1 + ". " + surname1 + " " + price);
+			
+			a++;
+			}
+		}
+		
+		
 	}
-}
