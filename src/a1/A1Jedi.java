28325a53c7bfb25010da45e575b0876d63449462
vydu <vydu@yingliangs-air.wireless-1x.unc.edu>
2019-09-01 22:55:54 -0400

    hoping

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -10,5 +10,55 @@ public class A1Jedi {
 
 		// Your code follows here.
 		
+		int items = scan.nextInt();
+		String[] prods = new String[items];
+		double[] prices = new double[items];
+		
+		for (int i = 0; i < items; i++) {
+			prods[i] = scan.next();
+			prices[i] = scan.nextDouble();
+		}
+		
+		int customers = scan.nextInt();
+		int a = 0; 
+		
+		int[] numBoughtArray = new int[items];
+		int[] custBoughtArray = new int[items];
+		
+		while (a < customers) {
+			String fname = scan.next();
+			String lname = scan.next();
+			
+			int load = scan.nextInt();
+			int b = 0;
+			while (b < load) {
+				int numBought = scan.nextInt();
+				String itemBought = scan.next();
+				int c = 0;
+				while (c < items) {
+					if (prods[c].equals(itemBought)) {
+						numBoughtArray[c] = numBoughtArray[c] + numBought;
+						custBoughtArray[c]++;
+					}
+					c++;
+				}
+				b++;
+			}
+		}
+		
+		System.out.println("LOL");
+		System.out.println(custBoughtArray[0] + custBoughtArray[1] + custBoughtArray[2]);
+		
+		int d = 0;
+		while (d < items) {
+			if (numBoughtArray[d] == 0) {
+				System.out.println("No customers bought " + prods[d]);
+			} else if (numBoughtArray[d] != 0) {
+				System.out.println(custBoughtArray[d] + " customers bought " + numBoughtArray[d] + " " + prods[d]);
+			}
+			d++;
+		}
+		
+		
 	}
 }
